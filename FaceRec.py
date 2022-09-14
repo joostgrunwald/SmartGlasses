ec3820fb018f72289d6484b90ed07aaa9ca5cce6
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-09-14 08:12:17 +0200

    Update FaceRec.py

--- FaceRec.py
+++ FaceRec.py
@@ -13,6 +13,10 @@ import numpy as num
 # time is only used to delay
 import time
 
+#mss and PIL are used for rapid screen captures
+from mss import mss
+from PIL import Image
+
 #! BRIEF EXPLANATION
 # This script is a simple face recognition script. It uses the face_recognition library to detect faces and compare them to a database of faces.
 # The database of faces is a folder with images of the people you want to recognize. The script will compare the faces in the database to the faces in the video footage.
@@ -33,8 +37,9 @@ NearestMode = True
 # If false don't display a bounding box around the face and show text output
 VisualMode = False
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+# If true work from the camera webcam
+# If false work from capturing parts of the screen
+CameraMode = False
 
 #! LOAD FACE DATABASE
 obama_image = fr.load_image_file("obama.jpg")
@@ -43,17 +48,19 @@ obama_face_encoding = fr.face_encodings(obama_image)[0]
 joost_image = fr.load_image_file("joost_train.jpg")
 joost_face_encoding = fr.face_encodings(joost_image)[0]
 
-# Create arrays of known face encodings and their names
-known_face_encodings = [
+# We create an array of the face encodings
+TrainedFaceEncodings = [
     obama_face_encoding,
     joost_face_encoding
 ]
-known_face_names = [
+
+# We create an array of the names of the people in the database
+TrainedFaces = [
     "Barack Obama",
     "Joost Grunwald"
 ]
 
-# Create variables to store the face locations and face encodings for the current frame of video
+# Two list to store face locations and encodings into
 FacesLocations = []
 FacesEncodings = []
 
@@ -61,20 +68,40 @@ FacesEncodings = []
 NamesOfFaces = []
 toProcess = True
 
+ScreenPart = {'left': 100, 'top': 100, 'width': 400, 'height': 400}
+
+video_capture = ""
+if CameraMode == True:
+    #Get video capture from the webcam
+    video_capture = cv2.VideoCapture(0)
+
+
 #! INDEFINITE FACE RECOGNITION LOOP
 while not StopFaceDetection:
 
-    # Get single frame from the video capture
-    ret, singleFrame = video_capture.read()
+    fourthFrameRGB = ""
+    ret = ""
+    singleFrame = ""
+    if CameraMode == True:
+        # Get single frame from the video capture
+        ret, singleFrame = video_capture.read()
+    else:
+        with mss() as sct:
+            Shot = sct.grab(ScreenPart)
+            img = Image.frombytes('RGB',(Shot.width,Shot.height),Shot.rgb)
+            cv2.imshow('test', num.array(img))
+            fourthFrameRGB = num.array(img)
+        
 
     # Only process half of frames
     if toProcess:
 
-        # We resize the frame to 1/4 of its original size to make the face detection faster
-        fourthFrame = cv2.resize(singleFrame, (0, 0), fx=0.25, fy=0.25)
+        if CameraMode:
+            # We resize the frame to 1/4 of its original size to make the face detection faster
+            fourthFrame = cv2.resize(singleFrame, (0, 0), fx=0.25, fy=0.25)
 
-        # We have to convert the image from BGR to RGB color format
-        fourthFrameRGB = fourthFrame[:, :, ::-1]
+            # We have to convert the image from BGR to RGB color format
+            fourthFrameRGB = fourthFrame[:, :, ::-1]
 
         # Process the current frame for faces and face encodings
         FacesLocations = fr.face_locations(fourthFrameRGB)
@@ -83,23 +110,23 @@ while not StopFaceDetection:
         NamesOfFaces = []
         for face_encoding in FacesEncodings:
             # Check if the face matches a face in our database
-            matches = fr.compare_faces(known_face_encodings, face_encoding)
+            matches = fr.compare_faces(TrainedFaceEncodings, face_encoding)
             name = "Face not found"
 
             if NearestMode:
                 # From the (possible) matches, get the face nearest to the face we are looking at
                 FaceDistance = fr.face_distance(
-                    known_face_encodings, face_encoding)
+                    TrainedFaceEncodings, face_encoding)
                 MatchInList = num.argmin(FaceDistance)
 
                 # If there is a proper match, save the name
                 if matches[MatchInList]:
-                    name = known_face_names[MatchInList]
+                    name = TrainedFaces[MatchInList]
             elif True in matches:
                 # Get the first match in our face encoding database
                 first_match_index = matches.index(True)
                 # Get the name corresponding to this first match
-                name = known_face_names[first_match_index]
+                name = TrainedFaces[first_match_index]
 
             NamesOfFaces.append(name)
 
