3a2e0b7ee29b10e8cda0e509f072e4886d40f431
Michelwv <82516079+Michelwv@users.noreply.github.com>
2022-10-04 10:22:20 +0200

    Updated location.py
    It now checks location every five minutes by using time counter. Still need to add messaging and fix it giving different locations even though we do not move.

--- location.py
+++ location.py
@@ -4,34 +4,70 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 import geopy.distance
 import time
+import datetime
+
+away = False
+maxDistance = 4
 
 def getLocation():
-    options = Options()
-    options.add_argument("--use-fake-ui-for-media-stream")
-    timeout = 20
-    driver = webdriver.Chrome(executable_path = './chromedriver.exe', chrome_options=options) #currently using 105.0.5195.52/
-    driver.get("https://mycurrentlocation.net/")
-    wait = WebDriverWait(driver, timeout)
-    time.sleep(3)
-    longitude = driver.find_elements("xpath", '//*[@id="longitude"]')  #changed this to make it compatible with current selenium version
-    longitude = [x.text for x in longitude]
-    longitude = str(longitude[0])
-    latitude = driver.find_elements("xpath", '//*[@id="latitude"]') #same change here
-    latitude = [x.text for x in latitude]
-    latitude = str(latitude[0])
-    driver.quit()
-    #geolocator = Nominatim(user_agent="geoapiExercises")
-    #location = geolocator.reverse(latitude+","+longitude)
-    #print(location)
-    latitude_home = '51.82546'
-    longitude_home = '5.86894'
-    coords_home = (latitude_home, longitude_home)
-    coords_cur = (latitude, longitude)
-    print (geopy.distance.geodesic(coords_home, coords_cur).km)
-    return (latitude,longitude)
-print(getLocation())
+    RecentlyCalled = False
+    while not RecentlyCalled:
+        # Create a timer that calls the script every 5 minutes
+        timer = datetime.datetime.now()
+        print(timer)
+        timer = timer + datetime.timedelta(minutes=5)
+        print(timer)
+        while(datetime.datetime.now() < timer):
+            i = 3
+            # we wait
+
+
+        print("we zijn er")
+        options = Options()
+        options.add_argument("--use-fake-ui-for-media-stream")
+        timeout = 20
+        driver = webdriver.Chrome(
+        executable_path='./chromedriver.exe', chrome_options=options)
+        driver.get("https://mycurrentlocation.net/")
+        wait = WebDriverWait(driver, timeout)
+        time.sleep(3)
+        longitude = driver.find_elements("xpath", '//*[@id="longitude"]')
+        longitude = [x.text for x in longitude]
+        longitude = str(longitude[0])
+        latitude = driver.find_elements("xpath", '//*[@id="latitude"]')
+        latitude = [x.text for x in latitude]
+        latitude = str(latitude[0])
+        driver.quit()
+
+        latitude_home = '51.82546'
+        longitude_home = '5.86894'
+        coords_home = (latitude_home, longitude_home)
+        coords_cur = (latitude, longitude)
+        distance = geopy.distance.geodesic(coords_home, coords_cur).km
+        print(distance)
+        if (distance > maxDistance):
+            print("oma is weg")
+            RecentlyCalled = True
+            # bericht sturen
+            # iets met counter
+        else:
+            print("oma is thuis")
+
+            # bericht sturen
+            # counter resetten
+
+
+def mainfunc():
+    while True:
+        #call once every 30 minutes
+        currentTime = datetime.datetime.now()
+        currentTime = currentTime + datetime.timedelta(minutes=30)
+        while datetime.datetime.now() < currentTime:
+            getLocation()
+
+if __name__ == "__main__":
+    mainfunc()
 
 #https://sites.google.com/a/chromium.org/chromedriver/downloads
 #https://codeburst.io/how-i-understood-getting-accurate-geolocation-using-python-web-scraping-and-selenium-7967d721587a
 #('51.82546', '5.86894')
-
