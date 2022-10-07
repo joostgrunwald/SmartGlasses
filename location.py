226c5cb54584acfe266cce1b5f87d937437b8a34
Michelwv <82516079+Michelwv@users.noreply.github.com>
2022-10-07 11:25:16 +0200

    Updated location.py
    Location now sends sms depending on location using Twilio.

--- location.py
+++ location.py
@@ -2,26 +2,43 @@ from selenium import webdriver
 from geopy.geocoders import Nominatim
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from email.message import EmailMessage
 import geopy.distance
+import smtplib
 import time
 import datetime
+
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
 away = False
 maxDistance = 4
 
 def getLocation():
+    sender = 'omaiskwijt@gmail.com'
+    receivers = ['omaiskwijt@gmail.com']
+
+    message = """From: From Person <omaiskwijt@gmail.com>
+    To: To Person <omaiskwijt@gmail.com>
+    Subject: Missing oma
+
+    Oma is missing, please help find her.
+    """
+
     RecentlyCalled = False
     while not RecentlyCalled:
         # Create a timer that calls the script every 5 minutes
         timer = datetime.datetime.now()
         print(timer)
-        timer = timer + datetime.timedelta(minutes=5)
+        timer = timer + datetime.timedelta(minutes=0.5)
         print(timer)
         while(datetime.datetime.now() < timer):
             i = 3
-            # we wait
-
-
+            #we wait
         print("we zijn er")
         options = Options()
         options.add_argument("--use-fake-ui-for-media-stream")
@@ -43,16 +60,28 @@ def getLocation():
         longitude_home = '5.86894'
         coords_home = (latitude_home, longitude_home)
         coords_cur = (latitude, longitude)
+        print(coords_cur)
         distance = geopy.distance.geodesic(coords_home, coords_cur).km
         print(distance)
         if (distance > maxDistance):
             print("oma is weg")
             RecentlyCalled = True
             # bericht sturen
-            # iets met counter
+            message = client.messages \
+                .create(
+                    body='Oma is kwijt',
+                    from_='+19706327688',
+                    to='+31613361986'
+                )
+            # iets met counterhttps://mycurrentlocation.net/
         else:
             print("oma is thuis")
-
+            message = client.messages \
+                .create(
+                    body='Oma is thuis',
+                    from_='+19706327688',
+                    to='+31613361986'
+                )
             # bericht sturen
             # counter resetten
 
@@ -71,3 +100,4 @@ if __name__ == "__main__":
 #https://sites.google.com/a/chromium.org/chromedriver/downloads
 #https://codeburst.io/how-i-understood-getting-accurate-geolocation-using-python-web-scraping-and-selenium-7967d721587a
 #('51.82546', '5.86894')
+#Omaiskwijt0309-#
