54b69cb0a1d3aaa39cd112586ad1d3150d59a5f7
Michelwv <82516079+Michelwv@users.noreply.github.com>
2022-12-04 19:49:10 +0100

    Delete medicines directory

--- medicines/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from medicines.models import Medicine
-
-admin.site.register(Medicine)
--- medicines/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from medicines.models import Medicine
-
-class MedicineForm(ModelForm):
-    class Meta:
-        model = Medicine
-        fields  = '__all__'
-        
